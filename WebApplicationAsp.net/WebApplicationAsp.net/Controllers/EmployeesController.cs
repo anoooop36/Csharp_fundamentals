59b537958ae9b05a8c350ce88f99e7faad80f82c
Chaudhary, Anoop <anoop.chaudhary@thermofisher.com>
2019-07-22 16:36:24 +0530

    Added message to response header of web api

--- WebApplicationAsp.net/WebApplicationAsp.net/Controllers/EmployeesController.cs
+++ WebApplicationAsp.net/WebApplicationAsp.net/Controllers/EmployeesController.cs
@@ -25,5 +25,32 @@ namespace WebApplicationAsp.net.Controllers
                 return entities.Employees.FirstOrDefault(e => e.ID == id);
             }
         }
+
+        public HttpResponseMessage Post([FromBody] Employee employee)
+        {
+            try
+            {
+                using (EmployeeDBEntities entities = new EmployeeDBEntities())
+                {
+                    entities.Employees.Add(employee);
+                    entities.SaveChanges();
+                    var message = Request.CreateResponse(HttpStatusCode.Created, employee);
+                    message.Headers.Location = new Uri(Request.RequestUri +"/"+ employee.ID.ToString());
+                    return message;
+                }
+            }
+            catch (Exception ex)
+            {
+
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+            }
+        }
     }
 }
+
+//{     "ID":"232",
+//     "FirstName": "Anoop",
+//     "LastName":" Chaudhary",
+//     "Gender":"M",
+//     "Salary":121
+//}
