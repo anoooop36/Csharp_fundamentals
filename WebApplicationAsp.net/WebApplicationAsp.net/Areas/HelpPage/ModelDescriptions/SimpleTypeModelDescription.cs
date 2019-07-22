b1322531aa8c40fe6868a6f6a2664c232c12176c
Chaudhary, Anoop <anoop.chaudhary@thermofisher.com>
2019-07-22 15:54:45 +0530

    web api with meadia formatter

---
+++ WebApplicationAsp.net/WebApplicationAsp.net/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace WebApplicationAsp.net.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ WebApplicationAsp.net/WebApplicationAsp.net/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebApplicationAsp.net.WebApiApplication" Language="C#" %>
