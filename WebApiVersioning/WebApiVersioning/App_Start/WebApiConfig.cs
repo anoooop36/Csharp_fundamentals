a8287ab0c63b739e5d15f7a4182e379527a7b110
Chaudhary, Anoop <anoop.chaudhary@thermofisher.com>
2019-07-31 23:05:45 +0530

    Versioning- custom media types

--- WebApiVersioning/WebApiVersioning/App_Start/WebApiConfig.cs
+++ WebApiVersioning/WebApiVersioning/App_Start/WebApiConfig.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Web.Http;
 using System.Web.Http.Dispatcher;
 using WebApiVersioning.CustomController;
@@ -29,6 +30,10 @@ namespace WebApiVersioning
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional}
             );
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("application/vnd.pragimtech.students.v1+json"));
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("application/vnd.pragimtech.students.v2+json"));
+            config.Formatters.XmlFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("application/vnd.pragimtech.students.v1+xml"));
+            config.Formatters.XmlFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("application/vnd.pragimtech.students.v2+xml"));
         }
     }
 }
--- WebApiVersioning/WebApiVersioning/CustomController/CustomControllerSelector.cs
+++ WebApiVersioning/WebApiVersioning/CustomController/CustomControllerSelector.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using System.Net.Http;
-using System.Web;
+using System.Text.RegularExpressions;
 using System.Web.Http;
 using System.Web.Http.Controllers;
 using System.Web.Http.Dispatcher;
@@ -11,50 +9,102 @@ namespace WebApiVersioning.CustomController
 {
     public class CustomControllerSelector : DefaultHttpControllerSelector
     {
-        HttpConfiguration _config;
-        public CustomControllerSelector(HttpConfiguration config): base(config)
+        private HttpConfiguration _config;
+
+        public CustomControllerSelector(HttpConfiguration config) : base(config)
         {
             _config = config;
         }
-        public override HttpControllerDescriptor SelectController(HttpRequestMessage request)
+
+        public override HttpControllerDescriptor
+            SelectController(HttpRequestMessage request)
         {
             var controllers = GetControllerMapping();
             var routeData = request.GetRouteData();
+
             var controllerName = routeData.Values["controller"].ToString();
-            string versioNumber = "1";
-            //var versionQueryString = HttpUtility.ParseQueryString(request.RequestUri.Query);
-            //if(versionQueryString["v"] != null)
+
+            string versionNumber = "1";
+
+            // Comment the code that gets the version number from Query String
+            // var versionQueryString =
+            //         HttpUtility.ParseQueryString(request.RequestUri.Query);
+            //if (versionQueryString["v"] != null)
             //{
-            //    versioNumber = versionQueryString["v"];
+            //    versionNumber = versionQueryString["v"];
             //}
+
+            // Get the version number from Custom version header
+
             //string customHeader = "X-StudentService-Version";
             //if (request.Headers.Contains(customHeader))
             //{
-            //    versioNumber = request.Headers.GetValues(customHeader).FirstOrDefault();
+            //    // If X-StudentService-Version:1 is specified twice in the request
+            //    // then in versionNumber variable will get a value of "1,1"
+            //    versionNumber = request.Headers.GetValues(customHeader).FirstOrDefault();
+            //    // Check if versionNumber string contains a comma, and take only
+            //    // the first number from the comma separated list of version numbers
+            //    if (versionNumber.Contains(","))
+            //    {
+            //        versionNumber = versionNumber.Substring(0, versionNumber.IndexOf(","));
+            //    }
             //}
-            var acceptHeader = request.Headers.Accept.Where(a => a.Parameters.Count(p => p.Name.ToLower() == "version") > 0);
+
+            // Get the version number from the Accept header
+
+            // Users can include multiple Accept headers in the request
+            // Check if any of the Accept headers has a parameter with name version
+            //var acceptHeader = request.Headers.Accept.Where(a => a.Parameters
+            //                    .Count(p => p.Name.ToLower() == "version") > 0);
+
+            //// If there is atleast one header with a "version" parameter
+            //if (acceptHeader.Any())
+            //{
+            //    // Get the version parameter value from the Accept header
+            //    versionNumber = acceptHeader.First().Parameters
+            //                    .First(p => p.Name.ToLower() == "version").Value;
+            //}
+
+            // Get the version number from the Custom media type
+
+            // Use regular expression for mataching the pattern of the media
+            // type. We have given a name for the matched group that contains
+            // the version number. This enables us to retrieve the version number 
+            // using the group name("version") instead of ZERO based index
+            string regex =
+                @"application\/vnd\.pragimtech\.([a-z]+)\.v(?<version>[0-9]+)\+([a-z]+)";
+
+            // Users can include multiple Accept headers in the request.
+            // Check if any of the Accept headers has our custom media type by
+            // checking if there is a match with regular expression specified
+            var acceptHeader = request.Headers.Accept
+                .Where(a => Regex.IsMatch(a.MediaType, regex, RegexOptions.IgnoreCase));
+            // If there is atleast one Accept header with our custom media type
             if (acceptHeader.Any())
             {
-                versioNumber = acceptHeader.First().Parameters.First(p => p.Name.ToLower() == "version").Value;
+                // Retrieve the first custom media type
+                var match = Regex.Match(acceptHeader.First().MediaType,
+                    regex, RegexOptions.IgnoreCase);
+                // From the version group, get the version number
+                versionNumber = match.Groups["version"].Value;
             }
-            if (versioNumber == "1")
-            {
-                controllerName = controllerName + "V1";
-            }
-            if (versioNumber == "2")
-            {
-                controllerName = controllerName + "V2";
-            }
+
             HttpControllerDescriptor controllerDescriptor;
-            if(controllers.TryGetValue(controllerName, out controllerDescriptor))
+            if (versionNumber == "1")
             {
-                return controllerDescriptor;
+                controllerName = string.Concat(controllerName, "V1");
             }
             else
             {
-                return null;
+                controllerName = string.Concat(controllerName, "V2");
             }
 
+            if (controllers.TryGetValue(controllerName, out controllerDescriptor))
+            {
+                return controllerDescriptor;
+            }
+
+            return null;
         }
     }
 }
