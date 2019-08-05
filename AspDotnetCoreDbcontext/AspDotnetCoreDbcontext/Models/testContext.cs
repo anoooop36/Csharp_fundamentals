57e5c53e4b8ad45ef3a2815e6df456af90be1efd
Chaudhary, Anoop <anoop.chaudhary@thermofisher.com>
2019-08-05 17:48:27 +0530

    Entity frameword + postgreSql

---
+++ AspDotnetCoreDbcontext/AspDotnetCoreDbcontext/Models/testContext.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace AspDotnetCoreDbcontext.Models
+{
+    public partial class testContext : DbContext
+    {
+        public testContext()
+        {
+        }
+
+        public testContext(DbContextOptions<testContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Item> Item { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseNpgsql("Host=localhost;Port=4332;Database=test;Username=postgres;Password=NextGenSAT");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("ProductVersion", "2.2.6-servicing-10079");
+
+            modelBuilder.Entity<Item>(entity =>
+            {
+                entity.ToTable("item");
+
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.Description).HasColumnName("description");
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasColumnName("name");
+            });
+        }
+    }
+}
---
+++ AspDotnetCoreDbcontext/AspDotnetCoreDbcontext/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using AspDotnetCoreDbcontext.Models;
+namespace AspDotnetCoreDbcontext
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            using (var db = new testContext())
+            {
+                // Creating a new item and saving it to the database
+                var newItem = new Item();
+                newItem.Name = "Red Apple";
+                newItem.Description = "Description of red apple";
+                db.Item.Add(newItem);
+                var count = db.SaveChanges();
+                Console.WriteLine("{0} records saved to database", count);
+                // Retrieving and displaying data
+                Console.WriteLine();
+                Console.WriteLine("All items in the database:");
+                foreach (var item in db.Item)
+                {
+                    Console.WriteLine("{0} | {1}", item.Name, item.Description);
+                }
+            }
+        }
+    }
+}
